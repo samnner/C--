3e44a09a87ec014da1c2809b38ebb497d8f5b646
samnner <you@example.com>
2022-06-28 19:28:23 +0800

    test

---
+++ 0judge/a021.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /*
+    long long a, b;
+    char o;
+    cin >> a >> o >> b;
+    cout << a << o << b;
+    */
+    long long a, b;
+    char o;
+    cin >> a >> o >> b;
+    if(o=='+')
+        cout << a+b;
+}
