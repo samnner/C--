e649ee2a665ee278225669eaa35024f70bf62458
samnner <you@example.com>
2022-07-13 18:08:27 +0800

    re

---
+++ leet code/Reverse Linked List.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL) return NULL;
+        ListNode *reverse = new ListNode(head->val);
+        while(head->next){
+            reverse = new ListNode(head->next->val,reverse);
+            head = head->next;
+        }
+        return reverse;
+    }
+};
